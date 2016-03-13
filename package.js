277046a9e59a96414da82794c1c579dd3e1f772f
Jorge Gonzalez <jorge@basgrani.com>
2016-03-13 22:52:36 +0100

    Update

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+    name: 'basgrani:bas-meteor-imgix',
+    version: '0.1.0',
+    summary: 'imgix server/client wrapped for meteor',
+    git: 'https://github.com/Basgrani-Org/bas-meteor-imgix'
+});
+
+Npm.depends({
+    "imgix-core-js": "1.0.3"
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+
+    // Vendor
+    api.addFiles('imgix.js/dist/imgix.min.js', ['client']);
+    api.addFiles('imgix.js/dist/imgix.jquery.min.js', ['client']);
+
+    // Server
+    api.addFiles('server.js', ['server']);
+
+    // Export
+    api.export('imgix', 'client');
+    api.export('imgix', 'server');
+});
